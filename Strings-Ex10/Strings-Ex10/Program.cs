c236f3db5fc34f5c4f78f626c00c4147dc8add47
MariusA <anca.ticau@gmail.com>
2019-06-06 23:19:01 +0300

    Exercise 10

---
+++ Strings-Ex10/Strings-Ex10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Strings_Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartCharacterA("Ana are mere");
+        }
+
+        //Write a method to check whether a string starts with specified characters
+
+        public static string StartCharacterA(string input)
+        {
+            if (input[0] == 'A')
+            {
+                Console.WriteLine("Yes, the string starts with \"a\"");
+            }
+            else
+            {
+                Console.WriteLine("No, the string doesn't start with \"a\"");
+            }
+            return input;
+        }
+    }
+}
